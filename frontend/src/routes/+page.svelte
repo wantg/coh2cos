e8d0099167520786205b8864ed3585f7c388bac1
wantg <wantg78@gmail.com>
2022-10-21 14:32:21 +0800

    show coh2 logo as ai avatar

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -33,7 +33,10 @@
           if (player.summary) {
             player.summary = JSON.parse(player.summary);
           } else {
-            player.summary = { country: '', avatar: '/images/coh2-logo.png' };
+            player.summary = { country: '' };
+          }
+          if (!player.summary.avatar) {
+            player.summary.avatar = '/images/coh2-logo.png';
           }
           if (player.stats) {
             player.stats = convertStatData(JSON.parse(player.stats), player.faction);
